a64046702cedbef9fabf418c0475c7531de337e3
gitlab runner <1136687@qq.com>
2020-10-29 15:40:20 +0800

    chore: 文件服务代理

--- src/webapp.ts
+++ src/webapp.ts
@@ -1,8 +1,16 @@
-export const proxy = () => {
+import { Options } from 'http-proxy-middleware';
+export const proxy = (): { [key: string]: Options } => {
   return {
     '/wxapi': {
       target: process.env.PROXY_TARGET,
       changeOrigin: true,
+    },
+    '/files': {
+      target: process.env.PROXY_TARGET,
+      changeOrigin: true,
+      headers: {
+        apikey: process.env.API_KEY,
+      },
     },
   };
 };
